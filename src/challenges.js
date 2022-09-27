9ae718c8179e427274f512fc60674136cd98a250
LucasS <lm.spizzirri@gmail.com>
2022-09-27 15:12:48 -0300

    2nd Desafio

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,10 @@
 // Desafio 1
 function compareTrue() {
-  // seu código aqui
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height) / 2;
 }
 
 // Desafio 3
